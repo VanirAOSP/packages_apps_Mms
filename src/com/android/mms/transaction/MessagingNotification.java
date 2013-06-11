a24104f70b1eb6a02eeadc40cda5ce201ece383b
Dan Trifan <jimsth@gmail.com>
2013-06-11 23:57:00 +0300

    Mms fix for Halo - taskon home. Credits go to PA
    Change-Id: I9b14e215c99bee99d6f08ae51ad1b41c78d10b2f
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -877,6 +877,7 @@ public class MessagingNotification {
             }
         }
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.setTaskOnHome(false);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -1272,6 +1273,7 @@ public class MessagingNotification {
         }
 
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.setTaskOnHome(false);
         if (allFailedInSameThread) {
             failedIntent = new Intent(context, ComposeMessageActivity.class);
             if (isDownload) {
